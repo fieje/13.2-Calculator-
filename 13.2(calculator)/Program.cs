6145d77d05f16fea81bd605df6f604d31996672d
jack <144657795+fieje@users.noreply.github.com>
2024-05-15 21:44:23 +0300

    Add files via upload

---
+++ 13.2(calculator)/Program.cs
@@ -0,0 +1,45 @@
+﻿//Калькулятор
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float num1, num2;
+        char oper;
+
+        Console.WriteLine("Enter operator (+, -, *, /): ");
+        oper = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter two numbers: ");
+        num1 = float.Parse(Console.ReadLine());
+        num2 = float.Parse(Console.ReadLine());
+
+        switch (oper)
+        {
+            case '+':
+                Console.WriteLine("Result: " + (num1 + num2));
+                break;
+            case '-':
+                Console.WriteLine("Result: " + (num1 - num2));
+                break;
+            case '*':
+                Console.WriteLine("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    Console.WriteLine("Result: " + (num1 / num2));
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero.");
+                }
+                break;
+            default:
+                Console.WriteLine("Error: Invalid operator.");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestCalculatorAddition()
+        {
+            float num1 = 5;
+            float num2 = 3;
+            float expectedResult = num1 + num2;
+
+            float result = PerformCalculation('+', num1, num2);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestCalculatorDivision()
+        {
+            float num1 = 10;
+            float num2 = 2;
+            float expectedResult = num1 / num2;
+
+            float result = PerformCalculation('/', num1, num2);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        private float PerformCalculation(char oper, float num1, float num2)
+        {
+            switch (oper)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        return num1 / num2;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException();
+                    }
+                default:
+                    throw new ArgumentException("Invalid operator.");
+            }
+        }
+    }
+}
